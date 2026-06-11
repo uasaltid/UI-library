8b2bc68724a6769cb3f3ad2342237be463189d39
Соль <147088527+uasalt@users.noreply.github.com>
2026-06-11 18:15:25 +0300

    dropbox loyaut fix

--- main.lua
+++ main.lua
@@ -601,7 +601,9 @@ function lib.create:dropbox(parent, items, selected:number, onchange)
 		select.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 		select.ZIndex = 4
 		select.BorderColor3 = lib.styles.dropbox.border
-		Instance.new("UIListLayout").Parent = select
+		local layaut = Instance.new("UIListLayout")
+		layout.SortOrder = Enum.SortOrder.LayoutOrder
+		layout.Parent = select
 		for index, title in ipairs(items) do
 			local option = Instance.new("TextButton")
 			option.Text = title
